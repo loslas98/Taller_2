3497b184fb0013d92c7550cb89c11a8dfc2a1d56
loslas98 <76071983+loslas98@users.noreply.github.com>
2021-04-28 10:31:53 -0400

    tercer commit testing

--- Taller02UnitTestingTest.java
+++ Taller02UnitTestingTest.java
@@ -3,14 +3,33 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class Taller02UnitTestingTest {
+   //@Test
+    //void leerArchivoTest() throws IOException {//deberia funcionar, los contenidos son los mismos pero me tira error igual.
+    //String contenidoEsperado = "\"Ají traga la lagartija\"\n" +
+    //"\"Un ejemplo de palíndromo es\"\n" +
+    // "\"Sex of foxes\"\n" +
+    // "\"123 456 654 321\"\n" +
+    //"\"The shortest palindromic words in English are nun, ewe and pip, among others\"\n" +
+    //"\"E la turba brutale\"\n" +
+    //"\"Este taller mezcla las 2 actividades que hicimos antes\"\n" +
+    //"\"127.0.0.1\"\n" +
+    //"\"Palíndromo no es una palabra palíndroma\"\n" +
+    //"\"Mensaje: Ha cometido un error!!!\"\n" +
+    //"\"Sie leben mit talg glatt im nebeleis\"\n" +
+    //"\"\"\n" +
+    //"\"A morte do galo no lago de Troma\"\n" +
+    //"\"Esta cadena sólo es para rellenar una línea del archivo\"\n" +
+    //"\"Nini me fete feminin\"\n" +
+    //"\"Iron maiden and Saxon are british heavy metal bands\"\n" +
+    //"\"No dejes de ponerte en contacto conmigo si tienes alguna duda\"\n" +
+    //"\"Was it a car or a cat I saw?\"\n" +
+    //"\"123-456-789\"\n" +
+    //"\"El bar es imán o zona miserable\"";
+        //String contenidoObtenido = Taller02UnitTesting.leerArchivo("cadenas.txt");
+    //assertEquals(contenidoEsperado,contenidoObtenido);
+    //}
 
-   @Test
-    void leerArchivoTest() {
-    }
-
-    @Test
-    void validarContenidoTest() {
-    }
+    
 
     @Test
     void añadirAarrayTest() {
@@ -52,4 +71,6 @@ class Taller02UnitTestingTest {
         String nulo = "";
         assertFalse(Taller02UnitTesting.verificarPalindromo(nulo));
     }
+
+   
 }
